821422343695f5d5300c60b781ec9b930c2be727
Hoang Dung <dungnguyen322003@gmail.com>
2023-04-05 09:59:46 +0700

    feat: app initialization

---
+++ src/context/CardProvider.tsx
@@ -0,0 +1,11 @@
+export type CartItemType = {
+  sku: string;
+  name: string;
+  price: number;
+  qty: number;
+};
+
+type CartStateType = { cart: CartItemType[] };
+
+// initial state
+const initCartState: CartStateType = { cart: [] };
